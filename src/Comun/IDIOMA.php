92e23a59f86fb5ad91c39d61c52e91d16f62894b
Víctor <{GIT_EMAIL}>
2024-12-09 12:18:53 +0100

    refactor: :sparkles: Added class IDIOMA

---
+++ src/Comun/IDIOMA.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lib\Comun;
+
+/**
+ * Idiomas validos
+ */
+final class IDIOMA extends Enum
+{
+    /**
+     * Idioma español
+     * @var int
+     */
+    const ING = 0;
+
+    /**
+     * Idioma inglés
+     * @var int
+     */
+    const ESP = 1;
+}
